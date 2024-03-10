cd2b05448f597329d99c129e1d5b755434d72a14
IonutNechita01 <ionut.nechita01@e-uvt.ro>
2024-03-10 17:17:30 +0200

    update bluetooth server receive data method

--- bluetooth_server/bluetooth_server.py
+++ bluetooth_server/bluetooth_server.py
@@ -6,20 +6,7 @@ from bottender.bot_tender import BotTender
 botTender = BotTender()
 class BluetoothServer:
     def __init__(self):
-        self.server = BltServer()
+        self.server = BltServer(onDataReceived)
 
-    def data_received(self, data):
-        if data == 'info':
-            self.server.send(botTender.getHost)
-
-    def client_connected(self):
-        print("Client connected")
-
-    def client_disconnected(self):
-        print("Client disconnected")
-
-    def start(self):
-        self.server.data_received = self.data_received
-        self.server.client_connected = self.client_connected
-        self.server.client_disconnected = self.client_disconnected
-        pause()
+def onDataReceived(data):
+    print("Received: " + data)
