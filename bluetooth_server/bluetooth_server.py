cc96741312762207fe9103ed0fe6fb72d2c9d62d
IonutNechita01 <ionut.nechita01@e-uvt.ro>
2024-03-19 15:10:53 +0200

    Add WiFi connection functionality

--- bluetooth_server/bluetooth_server.py
+++ bluetooth_server/bluetooth_server.py
@@ -1,4 +1,5 @@
 import json
+import os
 import subprocess
 from bluedot.btcomm import BluetoothServer
 
@@ -6,41 +7,47 @@ from bottender.bot_tender import BotTender
 
 botTender = BotTender()
 
+
 def connect_to_wifi(ssid, password):
     try:
-        connected_network = subprocess.run(['iwgetid'], capture_output=True, text=True)
-        if ssid in connected_network.stdout:
-            print("Already connected to", ssid)
-            return {"status": "Already connected"}
-
-        subprocess.run(['sudo', 'iw', 'dev', 'wlan0', 'disconnect'])
-
-        print("Connecting to", ssid)
-        subprocess.run(['iw', 'dev', 'wlan0', 'connect', ssid])
-
-        subprocess.run(['sleep', '5'])
-
-        password = password.encode('utf-8')
-        password_output = subprocess.run(['wpa_passphrase', ssid], capture_output=True, text=True, input=password)
-        with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'a') as f:
-            f.write('\n' + password_output.stdout)
-
-        subprocess.run(['sudo', 'wpa_supplicant', '-B', '-i', 'wlan0', '-c', '/etc/wpa_supplicant/wpa_supplicant.conf'])
-
-        subprocess.run(['sleep', '5'])
-
-        subprocess.run(['sudo', 'dhclient', '-r'])
-        subprocess.run(['sudo', 'dhclient'])
+        config_lines = [
+            'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev',
+            'update_config=1',
+            'country=US',
+            '\n',
+            'network={',
+            '\tssid="{}"'.format(ssid),
+            '\tpsk="{}"'.format(password),
+            '}'
+        ]
+        config = '\n'.join(config_lines)
         
-        return {"status": "Connected"}
+        # Ensure proper permissions
+        os.popen("sudo chmod a+w /etc/wpa_supplicant/wpa_supplicant.conf")
+        
+        # Write configuration to wpa_supplicant.conf
+        with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w") as wifi:
+            wifi.write(config)
+        
+        # Reconfigure wpa_supplicant
+        os.popen("sudo wpa_cli -i wlan0 reconfigure")
+        
+        # Check if an IP address is assigned
+        ip_output = subprocess.check_output(['ip', 'addr', 'show', 'wlan0']).decode()
+        if 'inet ' in ip_output:
+            return {"status": "connected", "message": "Connected to WiFi"}
+        else:
+            return {"status": "Error", "message": "Failed to obtain IP address."}
+    except IOError as e:
+        return {"status": "Error", "message": "IOError: {}".format(str(e))}
     except Exception as e:
         return {"status": "Error", "message": str(e)}
 
-    
+
 def prepareResponse(data):
     print(data)
     return json.dumps(data)
-    
+
 
 def startBluetoothServer():
     def onDataReceived(data):
@@ -50,15 +57,14 @@ def startBluetoothServer():
         print(path)
         print(data)
 
-        if path == 'info': 
+        if path == 'info':
             server.send(prepareResponse(botTender.toJson()))
 
-        if path == 'host': 
+        if path == 'host':
             server.send(prepareResponse(botTender.getHost()))
 
         if path == 'wifi-credentials':
             response = connect_to_wifi(data['ssid'], data['password'])
             server.send(prepareResponse(response))
-            
-    
+
     server = BluetoothServer(onDataReceived)
