bbdc291d9a439696cb84dacd32da0469ab78c6fc
IonutNechita01 <ionut.nechita01@e-uvt.ro>
2024-03-20 12:00:47 +0200

    Refactor WiFi connection method in bluetooth_server.py last one

--- bluetooth_server/bluetooth_server.py
+++ bluetooth_server/bluetooth_server.py
@@ -1,61 +1,44 @@
 import json
-import wifi
-import subprocess
+import time
+from pywifi import PyWiFi, const, Profile
 from bluedot.btcomm import BluetoothServer
 
 from bottender.bot_tender import BotTender
 
 botTender = BotTender()
 
-def connect_to_wifi1(ssid, password):
-    try:
-        # Check if a scheme exists before attempting to delete it
-        existing_scheme = wifi.Scheme.find('wlan0', '')
-        if existing_scheme:
-            existing_scheme.delete()
-
-        wifi_scanner = wifi.Cell.all('wlan0')
-        wifi_found = False
-        for cell in wifi_scanner:
-            if cell.ssid != ssid:
-                scheme = wifi.Scheme.for_cell('wlan0', ssid, cell, password)
-                scheme.save()
-                scheme.activate()
-                wifi_found = True
-                break
-        
-        if wifi_found:
-            return {
-                "status": "success",
-                "message": "Connected to WiFi: " + ssid
-            }
-        else:
-            return {
-                "status": "error",
-                "message": "WiFi network not found"
-            }
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": str(e)
-        }
-    
 def connect_to_wifi(ssid, password):
+    STATUS_CONNECTED = 0
+    STATUS_NETWORK_NOT_FOUND = 1
+    STATUS_INCORRECT_PASSWORD = 2
     try:
-        subprocess.run(["nmcli", "device", "disconnect", "wlan0"])
-        
-        subprocess.run(["nmcli", "device", "wifi", "connect", ssid, "password", password])
-        
-        return {
-            "status": "success",
-            "message": "Connected to WiFi: " + ssid
-        }
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": str(e)
-        }
-
+        wifi = PyWiFi()
+        iface = wifi.interfaces()[1]
+        iface.scan()
+        time.sleep(8)
+        network_found = False
+        for network_profile in iface.scan_results():
+            print(network_profile.ssid)
+            if network_profile.ssid == ssid:
+                network_found = True
+                break
+        assert network_found, STATUS_NETWORK_NOT_FOUND
+        iface.remove_all_network_profiles()
+        profile = Profile()
+        profile.ssid = ssid
+        profile.auth = const.AUTH_ALG_OPEN
+        profile.akm.append(const.AKM_TYPE_WPA2PSK)
+        profile.cipher = const.CIPHER_TYPE_CCMP
+        profile.key = password
+        iface.add_network_profile(profile)
+        iface.connect(profile)
+        time.sleep(5)
+        assert iface.status() == const.IFACE_CONNECTED, STATUS_INCORRECT_PASSWORD
+        print("success")
+        return {'status': STATUS_CONNECTED}
+    except Exception as eStatus:
+        print(eStatus)
+        return {'status': eStatus}
 
 def prepareResponse(data):
     print(data)
