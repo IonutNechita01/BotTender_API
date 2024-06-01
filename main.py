2eee1ce8452f0387dfd464f32544fcc433b9aea3
IonutNechita01 <ionut.nechita01@e-uvt.ro>
2024-06-01 18:02:46 +0300

    Start ui on a separete thread

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import socket
 from bottender.bot_tender_ui import BotTenderUI
 from bottender.bot_tender import BotTender
+from threading import Thread
 
 
 from bluetooth_server.bluetooth_server import startBluetoothServer
@@ -41,7 +42,8 @@ def start_bluetooth_server():
 if __name__ == "__main__":
     bot_tender = BotTender()
     app = BotTenderUI(bot_tender)
-    app.mainloop()
+    ui_thread = Thread(target=app.mainloop)
+    ui_thread.start()
     config = read_config()
     host = get_local_ip()
     http_port = config.get("http_port", 8000)
